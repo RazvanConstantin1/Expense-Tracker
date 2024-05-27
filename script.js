adb6a5732275187af7c6ab4c98d94765772969d5
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-05-27 21:01:18 +0200

    implemented deleteTransaction fn

--- script.js
+++ script.js
@@ -34,33 +34,44 @@ const list = document.getElementById("transactionList");
 const status = document.getElementById("status");
 
 function renderList() {
-  list.innerHTML = ``;
+  list.innerHTML = "";
 
+  status.textContent = "";
   if (transactions.length === 0) {
-    status.textContent = "No Transactions";
+    status.textContent = "No transactions.";
     return;
   }
 
-  transactions.forEach(({ id, name, date, amount, type }) => {
+  transactions.forEach(({ id, name, amount, date, type }) => {
+    const sign = "income" === type ? 1 : -1;
+
     const li = document.createElement("li");
 
     li.innerHTML = `
-    <div class ="name">
-    <h4>${name}</h4>
-    <p>${new Date(date).toLocaleDateString()}</p>
-    </div>
+      <div class="name">
+        <h4>${name}</h4>
+        <p>${new Date(date).toLocaleDateString()}</p>
+      </div>
 
+      <div class="amount ${type}">
+        <span>${formatter.format(amount * sign)}</span>
+      </div>
     
-     
-    <div class = "amount ${type}" >
-    <span>${formatter.format(amount)}</span>
-    </div>
-    
-    <div class ="action">
-    </div>
+      <div class="action">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" onclick="deleteTransaction(${id})">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      </div>
     `;
 
     list.appendChild(li);
   });
 }
 renderList();
+
+function deleteTransaction(id) {
+  const index = transactions.findIndex((trx) => trx.id === id);
+  transactions.splice(index, 1);
+
+  renderList();
+}
